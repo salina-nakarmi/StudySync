f514c0bd8e1d0c7a21a72a717e9b25fd6db66494
melinap9omu <aniepomu@gmail.com>
2025-10-24 16:25:23 +0545

    Database is connected to Neon

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 def main():
-    print("Hello from backend!")
+    print("Hello from backend")
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        print(" DATABASE_URL Loaded successfully.")
+        print(f" Using database:{database_url.split('@'[-1])}")
 
 
-if __name__ == "__main__":
+    else:
+        print("CRITICAL ERROR : DATABASE_URL not found!")
+        print(" Please check your .env file and ensure 'DATABASE_URL' is set") 
+
+
+if __name__=="__main__":
     main()
