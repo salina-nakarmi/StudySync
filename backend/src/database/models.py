aeca92851051641e3cc2ef1150b5f02b4bd80e83
melinap9omu <aniepomu@gmail.com>
2025-10-25 13:54:17 +0545

    Added models for Groups

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,18 +1,35 @@
-from sqlalchemy import Column, Integer, String, DateTime, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+from .database import Base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import ForeignKey, func
 
-##Example SQLAlchemy model for a User table
-
-engine = create_engine('sqlite:///app.db', echo=True)
-Base = declarative_base()
-
-class User(Base):
+class Users(Base):
     __tablename__ = 'users'
-    
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
 
+    user_id:Mapped[int]=mapped_column(primary_key=True)
+    username:Mapped[str]= mapped_column(unique=True)
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    email:Mapped[str]= mapped_column(unique=True)
+    phone_no: Mapped[str]
+    created_at:Mapped[datetime] = mapped_column(default=func.now())
+    updated_at:Mapped[datetime] = mapped_column(default=func.now(), onupdate=func.now())
+
+
+class Groups{
+    __tablename__ = 'groups'
+
+    id:Mapped[int]=mapped_column(primary_key=True)
+    leader_id:Mapped[int]= mapped_column(ForeignKey('users.user_id'))
+    group_name:Mapped[str]
+
+
+
+}
+    
+
+
+
+    
+ 
