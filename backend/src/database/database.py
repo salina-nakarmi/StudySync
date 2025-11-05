fde7dd87b500b072303f3e30eebccee55e00d174
salina-nakarmi <salina123karmi@gmail.com>
2025-11-05 21:44:22 +0545

    corrected the url portion

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -6,17 +6,16 @@ from sqlalchemy.orm import DeclarativeBase
 database_url =os.getenv("DATABASE_URL")
 
 if database_url:
-    asyn_database_url= database_url.replace("postgresql//:", "postgresql+asyncjg//:")
+    database_url = database_url.replace("postgresql://", "postgresql+asyncpg://")
 
 else:
-    ValueError("DATABASE_URL environmental Variable is not set")
+    raise ValueError("DATABASE_URL environmental Variable is not set")
 
 
-engine=create_async_engine(asyn_database_url, echo=True)
+engine=create_async_engine(database_url, echo=True)
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 async_session_local =async_sessionmaker(engine, expire_on_commit=False
 )
-
