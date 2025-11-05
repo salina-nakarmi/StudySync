e92c8649f0f0be7d687c6c5985b9828b5a0948a9
salina-nakarmi <salina123karmi@gmail.com>
2025-11-05 21:47:30 +0545

    added the route for streak

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routes import Dashboard
+from .routes import Dashboard, streaks
 
 app = FastAPI()
 
@@ -14,3 +14,5 @@ app.add_middleware(
 
 
 app.include_router(Dashboard.router, prefix="/api")
+app.include_router(streaks.router)
+
