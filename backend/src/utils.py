ff824e5181b2fa8e899b991a04a494537bc6d34d
salina-nakarmi <salina123karmi@gmail.com>
2025-12-30 10:35:37 +0545

    installed claudinary and configured in utils.py

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -3,8 +3,18 @@ from fastapi import HTTPException, Request
 from clerk_backend_api import Clerk, AuthenticateRequestOptions
 import os
 from dotenv import load_dotenv
+import cloudinary
 
 load_dotenv()
+
+
+# Configure Cloudinary on app startup
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True  # Use HTTPS
+)
 
 
 clerk_sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
