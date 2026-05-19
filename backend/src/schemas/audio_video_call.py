2cb213d328e6127d3e9d2a19797e8f876691646f
user <user@users-MacBook-Pro.local>
2026-05-19 02:40:04 -0700

    added scheme for group audio and video call

---
+++ backend/src/schemas/audio_video_call.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class TokenRequest(BaseModel):
+    room_name: str
+    participant: str
+
+class CallHistoryResponse(BaseModel):
+    id: int
+    group_id: int
+    started_at: datetime
+    ended_at: datetime
+    participants: list[str]
+
+    class Config:
+        from_attributes = True    
+
+class KickParticipant(BaseModel):
+    room_name: str
+    participant: str        
+
+class MuteParticipant(BaseModel):
+    room_name: str
+    participant: str
+    track_sid: str
+    mute: bool
+
+class RoomRequest(BaseModel):
+    room_name: str
+    max_participants: InterruptedError
